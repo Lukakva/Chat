5937644c7c7f744d1f303e2934ae52be61ab10a6
Lukakva <lukakvavilashvili@hotmail.com>
2016-02-02 16:49:24 +0400

    Initial Commit

---
+++ public/main.js
@@ -0,0 +1,163 @@
+// chat section
+function sendMessage() {
+	var message = chatInput.value;
+	if (message != "") {
+		// if message is not an empty string
+		// send it
+		socket.emit("message", message);
+		// empty the input
+		chatInput.value = "";
+
+		createMessageNode(username, message, username);
+	}
+}
+
+function createMessageNode(from, message, currentUser) {
+	var node = document.createElement("div");
+	node.innerHTML =
+	'<div class="chat-message-container">' +
+		'<div class="chat-message-info">' +
+			'<span class="chat-message-from">' + from + '</span>' +
+		'</div>' +
+		'<div class="chat-message">' +
+			message + 
+		'</div>' +
+	'</div>';
+
+	if (currentUser === from) {
+		node.querySelector(".chat-message-from").classList.add("current-user");
+	}
+
+	chatWindow.appendChild(node.children[0]);
+	chatWindow.scrollTop = chatWindow.scrollHeight;
+}
+
+function createUserLeftJoinedNode(user, leftOrJoined) {
+	var node = document.createElement("div");
+	node.classList.add("chat-user-left-joined");
+
+	node.innerHTML = user + " has " + leftOrJoined + " the chat";
+
+	chatWindow.appendChild(node);
+	chatWindow.scrollTop = chatWindow.scrollHeight;
+}
+
+function recreateChatFromHistory() {
+	for (var i = 0; i < chatHistory.length; i++) {
+		var chatStage = chatHistory[i];
+		var action = chatStage.action;
+
+		switch (action) {
+			case "newUserJoined":
+				createUserLeftJoinedNode(chatStage.username, "joined");
+				break;
+			case "userLeft":
+				createUserLeftJoinedNode(chatStage.username, "left");
+				break;
+			case "message":
+				createMessageNode(chatStage.from, chatStage.message, username);
+				break;
+		}
+	}
+}
+
+function createUserTyping(username) {
+	var node = document.createElement("div");
+	node.classList.add("user-typing");
+	node.classList.add(username);
+	node.innerHTML = username + " is typing...";
+
+	usersTyping.appendChild(node);
+}
+
+function removeUserTyping(username) {
+	var userTyping = document.querySelector(".user-typing." + username);
+	usersTyping.removeChild(userTyping);
+}
+
+var ENTER_KEYCODE = 13;
+var sendButton = document.querySelector(".chat-send");
+var chatInput = document.querySelector(".chat-input");
+var chatWindow = document.querySelector(".chat-window");
+var chatHistory = [];
+var usersTyping = document.querySelector(".users-typing");
+var username = window.prompt("Enter your username");
+
+// don't send "User is typing" to server on every input
+var sentIsTyping = false;
+
+chatInput.addEventListener("input", function() {
+	if (!sentIsTyping) {
+		socket.emit("typingStart");
+	}
+	sentIsTyping = true;
+});
+
+chatInput.addEventListener("inputEnd", function() {
+	sentIsTyping = false;
+	socket.emit("typingEnd");
+});
+
+var inputEndTimeout;
+
+chatInput.addEventListener("input", function() {
+	clearTimeout(inputEndTimeout);
+	inputEndTimeout = setTimeout(function() {
+		chatInput.dispatchEvent(new Event("inputEnd"));
+	}, 500);
+});
+
+document.addEventListener("keydown", function(e) {
+	var keyCode = e.keyCode || e.which;
+
+	if (keyCode === ENTER_KEYCODE) {
+		sendButton.classList.add("active");
+	}
+});
+
+document.addEventListener("keyup", function(e) {
+	var keyCode = e.keyCode || e.which;
+
+	if (keyCode === ENTER_KEYCODE) {
+		sendButton.classList.remove("active");
+		sendMessage();
+	}
+});
+
+sendButton.addEventListener("click", sendMessage);
+sendButton.addEventListener("touchend", sendMessage);
+
+// socket section
+	var socket = io();
+	var connectionCount = 0;
+
+	socket.on("welcome", function(data) {
+		connectionCount = data.connectionCount;
+		chatHistory = data.history;
+
+		recreateChatFromHistory();
+	});
+
+	socket.on("message", function(data) {
+		createMessageNode(data.from, data.message, username);
+	});
+
+	socket.on("alreadyLoggedIn", function() {
+		username = window.prompt("Username " + username + " is already logged in. Please try another username");
+		socket.emit("login", username);
+	});
+
+	socket.on("newUserJoined", function(data) {
+		connectionCount = data.connectionCount;
+		createUserLeftJoinedNode(data.username, "joined");
+	});
+
+	socket.on("userLeft", function(data) {
+		connectionCount = data.connectionCount;
+		createUserLeftJoinedNode(data.username, "left");
+	});
+
+	socket.emit("login", username);
+
+	socket.on("typingStart", createUserTyping);
+	socket.on("typingEnd", removeUserTyping);
---
+++ server.js
@@ -0,0 +1,97 @@
+// Import libs
+var express = require("express");
+var app = express();
+var fs  = require("fs");
+var httpServer = app.listen(8080, function(request, response) {
+	console.log("Server successfully launched and is listening on port 8080");
+});
+var io = require("socket.io").listen(httpServer);
+
+// use "public" folder as static one
+app.use(express.static(__dirname + "/public"));
+
+var connectionCount = 0;
+var usernames = [];
+var history = [];
+
+io.on("connection", function(socket) {
+	socket.emit("welcome", {
+		connectionCount: connectionCount,
+		history: history
+	});
+
+	socket.on("message", function(message) {
+		// on new message request, if username is not logged in, throw an error
+		if (!socket.username) {
+			return socket.emit("error", {
+				message: "You have to be logged in to send messages"
+			});
+		}
+
+		history.push({
+			action: "message",
+			message: message,
+			from: socket.username
+		});
+
+		// otherwise send a message to everyone
+		socket.broadcast.emit("message", {
+			message: message,
+			from: socket.username
+		});
+	});
+
+	socket.on("typingStart", function() {
+		console.log(socket.username + " is typing");
+		socket.broadcast.emit("typingStart", socket.username);
+	});
+
+	socket.on("typingEnd", function() {
+		console.log(socket.username + " has ended typing");
+		socket.broadcast.emit("typingEnd", socket.username);
+	});
+
+	socket.on("login", function(username) {
+		// if that username is already logged in, throw an error
+		if (usernames.indexOf(username) !== -1) {
+			return socket.emit("alreadyLoggedIn");
+		}
+
+		// otherwise add current username to array
+		usernames.push(username);
+
+		socket.username = username;
+
+		// notify other usernames that new user has joined the chat
+		socket.broadcast.emit("newUserJoined", {
+			username: username,
+			connectionCount: connectionCount++
+		});
+
+		history.push({
+			action: "newUserJoined",
+			username: username
+		});
+	});
+
+	socket.on("disconnect", function() {
+		// if the user was logged in
+		if (socket.username) {
+			socket.broadcast.emit("userLeft", {
+				username: socket.username,
+				connectionCount: connectionCount--
+			});
+
+			history.push({
+				action: "userLeft",
+				username: socket.username
+			});
+
+			// this statement below will always be true (I think)
+			// but extra check is not bad
+			if (usernames.indexOf(socket.username) > -1) {
+				usernames.splice(usernames.indexOf(socket.username), 1);
+			}
+		}
+	});
+});
